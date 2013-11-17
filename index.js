f0cb1593ae747d51f9ce9fb6279bd74a18b4ec67
Kenan Yildirim <kenan@kenany.me>
2013-11-17 14:01:30 -0500

    Fix `null` being returned for empty strings

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 module.exports = function(value) {
-  var caps = 0;
-
-  var index = -1;
   var length = value.length;
+
+  // If `value` is an empty string, then it will have 0 caps and a length of 0,
+  // which, when divided, will yield `NaN`. A string with a length of 0 will
+  // obviously have no caps.
+  if (!length) {
+    return 0;
+  }
+
+  var caps = 0;
+  var index = -1;
   while (++index < length) {
     var chr = value[index];
     if (chr.toLowerCase() !== chr && chr.toUpperCase() === chr) {
